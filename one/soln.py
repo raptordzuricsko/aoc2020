a2b3b535104e4e64fed485e9dcbedeac9e5f0de9
Raptor Dz <gregdzuricsko@gmail.com>
2020-12-05 17:36:29 -0500

    day one part two

--- one/soln.py
+++ one/soln.py
@@ -5,6 +5,8 @@ def test():
     import pdb; pdb.set_trace()
     for x in lines:
         for y in lines:
-            if x + y == 2020:
-                print(x * y)
+            for z in lines:
+                if x + y + z == 2020:
+                    print(x * y * z)
+                    print(str(x) + ',' + str(y) + ',' + str(z))
     print('test')
