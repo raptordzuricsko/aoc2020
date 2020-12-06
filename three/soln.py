30b736dbe01d74b1dddd18ee37d16590ecf4b864
Raptor Dz <gregdzuricsko@gmail.com>
2020-12-05 19:32:46 -0500

    day three part two

--- three/soln.py
+++ three/soln.py
@@ -4,15 +4,23 @@ def test():
     with open('input.txt') as f:
         lines = f.read().splitlines()
 
-    tree_count = 0
-    # start at 1, 3
-    x = 1
-    y = 3
-    while x < len(lines):
-        if lines[x][y] == '#':
-            tree_count += 1
-        x += 1
-        # meh, seems easier to jump to the front than extend matrices
-        y = (y + 3) % len(lines[0])
-    return tree_count
+    tree_counts = []
+    # list of how many to move down, then how many to move right
+    for pair in [[1,1], [1, 3], [1, 5], [1, 7], [2, 1]]:
+        #start at the coordinates from the first pair, not 0,0
+        x = pair[0]
+        y = pair[1]
+        tree_count = 0
+        while x < len(lines):
+            if lines[x][y] == '#':
+                tree_count += 1
+            x += pair[0]
+            # meh, seems easier to jump to the front than extend matrices
+            y = (y + pair[1]) % len(lines[0])
+        tree_counts.append(tree_count)
+    tree_multiplied_answer = 1
+    for i in tree_counts:
+        tree_multiplied_answer *= i
+        
+    return tree_multiplied_answer
 
