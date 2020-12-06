0f340eaa20e6521987ef39d4fb6307d01d0c1a02
Raptor Dz <gregdzuricsko@gmail.com>
2020-12-05 23:10:40 -0500

    day four part two

--- four/soln.py
+++ four/soln.py
@@ -9,6 +9,7 @@ def test():
     valid_pp_count = 0
     passports = []
     passport = {}
+    valid_pps = []
 
     for line in lines:
         # have we read in the whole passport?
@@ -18,6 +19,7 @@ def test():
             passports.append(passport.copy())
             if valid_pp:
                 valid_pp_count += 1
+                valid_pps.append(passport.copy())
             passport = {}
 
         else:
@@ -27,17 +29,60 @@ def test():
                 q_k_p = quality.split(':')
                 passport[q_k_p[0]] =  q_k_p[1]
         
-
     return valid_pp_count
 
 
 
 required_qualities = ['byr', 'iyr', 'eyr', 'hgt',  
-    'hcl', 'ecl', 'pid'] 
+        'hcl', 'ecl', 'pid'] 
 optional_qualities = ['cid']
 def validate_passport(passport):
     for req_quality in required_qualities:
         if req_quality not in passport.keys():
             return False
+
+    try:
+        if not (len(passport['byr']) == 4 and 
+                int(passport['byr']) >= 1920 and 
+                int(passport['byr']) <= 2002):
+            return False
+        if not (len(passport['iyr']) == 4 and 
+                int(passport['iyr']) >= 2010 and 
+                int(passport['iyr']) <= 2020):
+            return False
+        if not (len(passport['eyr']) == 4 and 
+                int(passport['eyr']) >= 2020 and 
+                int(passport['eyr']) <= 2030):
+            return False
+
+        #hgt validation
+        if passport['hgt'].endswith('cm'):
+            hgt = int(passport['hgt'].replace('cm',''))
+            if not (hgt >= 150 and hgt <= 193):
+                print('filtering out height!', hgt)
+                return False
+        elif passport['hgt'].endswith('in'):
+            hgt = int(passport['hgt'].replace('in',''))
+            if not (hgt >= 59 and hgt <= 76):
+                print('filtering out height2!', hgt)
+                return False
+        else: return False
+
+        #hcl validation #123def
+        if len(passport['hcl']) != 7 or passport['hcl'][0] != '#':
+            print('filtering out hcl', passport['hcl'])
+            return False
+        #check for hexadecimal >:)
+        if not int(passport['hcl'][1:7], 16):
+            print('filtering out hcl2', passport['hcl'])
+            return False
+
+        if passport['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+            return False
+        if not (len(passport['pid']) == 9 or not int(passport['pid'])):
+            return False
+    except ValueError as e:
+        print(e)
+        return False
     return True
         
