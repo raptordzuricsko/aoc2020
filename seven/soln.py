4b63e067da3db82effcd9f65c0128cccd7511498
Raptor Dz <gregdzuricsko@gmail.com>
2020-12-13 21:35:34 -0500

    day seven part one. is bag?

---
+++ seven/soln.py
@@ -0,0 +1,68 @@
+def main(): 
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    rules = {}
+
+    for line in lines:
+        rule = line.split(' bags contain ')
+        defining_color = rule[0]
+        suffixes = rule[1].split(', ')
+        rules[defining_color] = {'rules' : [], 'eventually_contains_gold': False}
+        for suffix in suffixes:
+            num_color_bags = suffix.split(' ')
+            num = num_color_bags[0]
+            color = ' '.join(num_color_bags[1:len(num_color_bags) - 1])
+            # not needed
+            # bags = num_color_bags[len(num_color_bags) - 1].strip('.')
+            # print(f'num is {num}, color is {color}')
+            rules[defining_color]['rules'].append({
+                'num': num,
+                'receiving_color': color
+            })
+        
+        #rules should now look like 
+        # 'posh_black' = {'rules': 
+        # [{'num': '3', 'receiving_color': 'dark lavender'}, 
+        # {'num': '3', 'receiving_color': 'mirrored coral'}, 
+        # {'num': '1', 'receiving_color': 'dotted chartreuse'}]
+        # }
+    
+    for key in rules.keys():
+        populate_gold_contains(rules, key)
+
+    keys = []
+    for key in rules.keys():
+        if rules[key]['eventually_contains_gold'] == True:
+            keys.append(key)
+    keys.sort()
+    import pdb; pdb.set_trace()
+    for key in keys:
+        print(key)
+
+    return len(keys)
+
+def populate_gold_contains(rules, current_color):
+    rule = rules[current_color]
+    for sub_rule in rule['rules']:
+        if sub_rule['receiving_color'] == 'shiny gold':
+            rule['eventually_contains_gold'] = True
+            return True
+            
+        if rule['eventually_contains_gold'] == True:
+            rule['eventually_contains_gold'] = True
+            return True
+    for sub_rule in rule['rules']:
+        if sub_rule['num'] != 'no':
+            rule['eventually_contains_gold'] = \
+                populate_gold_contains(rules, sub_rule['receiving_color'])
+    for sub_rule in rule['rules']:
+        if sub_rule['num'] != 'no' and rules[sub_rule['receiving_color']]['eventually_contains_gold'] == True:
+            rule['eventually_contains_gold'] = True
+            return True
+    return False
+    
+
+        
+
+            
